8dc45f5b27e1bd8ae05935adb39b244cb5175d99
LL8719 <arsgatslr_87@yahoo.com>
2023-03-31 19:03:32 -0700

    set up associations in models/index.js

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+User.hasMany(Post, {
+	foreignKey: 'user_id',
+});
+User.hasMany(Comment, {
+	foreignKey: 'user_id',
+	onDelete: 'cascade',
+	hooks: true,
+});
+Post.belongsTo(User, {
+	foreignKey: 'user_id',
+	onDelete: 'cascade',
+});
+Post.hasMany(Comment, {
+	foreignKey: 'post_id',
+	onDelete: 'cascade',
+	hooks: true,
+});
+Comment.belongsTo(User, {
+	foreignKey: 'user_id',
+	onDelete: 'cascade',
+	hooks: true,
+});
+Comment.belongsTo(Post, {
+	foreignKey: 'post_id',
+	onDelete: 'cascade',
+	hooks: true,
+});
+
+module.exports = { User, Post, Comment };
