58c481fab0c545c13ed8ea1a9ded5f39903897de
LL8719 <arsgatslr_87@yahoo.com>
2023-04-07 18:07:39 -0700

    trying to get comments to work

--- utils/auth.js
+++ utils/auth.js
@@ -1,7 +1,9 @@
 const withAuth = (req, res, next) => {
 	if (!req.session.logged_in) {
+		console.log('User is not logged in');
 		res.redirect('/login');
 	} else {
+		console.log('User is logged in');
 		next();
 	}
 };
