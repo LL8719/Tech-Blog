617659ef6f329c99f01a242bfdd54232c911290a
LL8719 <arsgatslr_87@yahoo.com>
2023-04-11 11:03:33 -0700

    fixed ability to modify post and added visibility of comments onto dashboard

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -17,4 +17,23 @@ router.post('/', withAuth, async (req, res) => {
 	}
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+	try {
+		const postData = await Comment.destroy({
+			where: {
+				id: req.params.id,
+				user_id: req.session.user_id,
+			},
+		});
+
+		if (!postData) {
+			res.status(404).json({ message: 'No Comment found with this id!' });
+			return;
+		}
+
+		res.status(200).json(postData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 module.exports = router;
--- controllers/api/post.js
+++ controllers/api/post.js
@@ -56,20 +56,4 @@ router.put('/:id', withAuth, async (req, res) => {
 	}
 });
 
-// router.post('/:id/comments', withAuth, async (req, res) => {
-// 	try {
-// 		if (req.session) {
-// 			const commentData = await Comment.create({
-// 				comment_text: req.body.comment_text,
-// 				post_id: req.params.id,
-// 				user_id: req.session.user_id,
-// 			});
-// 			res.json(commentData);
-// 		}
-// 	} catch (err) {
-// 		console.log(err);
-// 		res.status(400).json(err);
-// 	}
-// });
-
 module.exports = router;
